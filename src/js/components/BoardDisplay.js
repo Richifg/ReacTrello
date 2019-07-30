f71c7d6dd22cabee0e033b1e5993513472fd88e5
richifg <richifg@gmail.com>
2019-07-30 14:14:04 -0600

    added navigation to BoardDisplay

--- src/js/components/BoardDisplay.js
+++ src/js/components/BoardDisplay.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 
 const BoardDisplay = ({ name, color, onClick }) => (
   <div className="col-lg-auto col-md-4 col-6">
-    <button
-      type="button"
+    <a
+      href={`#/board/${name}`}
+      role="button"
       className={`btn btn-block btn-primary mb-4 board-display bg-${color}`}
       onClick={onClick}
     >
       <p className="board-display-title">{name}</p>
-    </button>
+    </a>
   </div>
 );
 
