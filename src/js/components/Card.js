6ddb560585696497027fd341f06a5a268cfb76b0
richifg <richifg@gmail.com>
2019-07-30 20:16:57 -0600

    styling

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Card = ({ description }) => (
-  <div className="card card-display mb-1 px-2 py-1 bg-light">
-    <p>{description}</p>
+  <div className="card card-display mb-2 px-2 py-1 bg-light shadow-b">
+    <p className="mb-0">{description}</p>
   </div>
 );
 
