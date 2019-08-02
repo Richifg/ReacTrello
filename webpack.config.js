89ee1046560157b79d7f98d8958f4857f2a37f9a
richifg <richifg@gmail.com>
2019-08-01 22:24:38 -0600

    changed ouput folder name to 'public'

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
 
   },
