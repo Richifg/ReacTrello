46ed2e0163fd471d5dd15c15f1d5a86a8a83eb45
richifg <richifg@gmail.com>
2019-08-06 14:22:03 -0600

    added basic local storage

--- src/js/redux/store.js
+++ src/js/redux/store.js
@@ -4,4 +4,40 @@ import rootReducer from './reducers/rootReducer';
 // actives redux dev tools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(rootReducer, composeEnhancers());
+
+// read and save functions for local storage
+
+const saveState = (state) => {
+  const data = {
+    boards: state.boards, cards: state.cards, lists: state.lists,
+  };
+  try {
+    const serializedData = JSON.stringify(data);
+    localStorage.setItem('state', serializedData);
+  } catch (e) {
+    throw e;
+  }
+};
+
+const readState = () => {
+  try {
+    const serializedData = localStorage.getItem('state');
+    if (serializedData) {
+      const data = JSON.parse(serializedData);
+      return { ...data, ui: { color: 'blue', img: null } };
+    } return undefined;
+  } catch (e) {
+    return undefined;
+  }
+};
+
+
+const store = createStore(rootReducer, readState(), composeEnhancers());
+
+store.subscribe(() => {
+  const state = store.getState();
+  console.log('saving state');
+  saveState(state);
+});
+
+export default store;
