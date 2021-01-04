1d494d8cdd5aa061510719b715705094f6ec2d52
richifg <richifg@gmail.com>
2021-01-03 18:25:37 -0600

    added favicon

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+
 import './icon-library';
+import '../assets/favicon.ico';
 import 'bootstrap/js/dist/modal';
 import '../scss/main.scss';
 
