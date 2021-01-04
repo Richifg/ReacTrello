de469d02e73e0b97f53f35724538b32858456867
richifg <richifg@gmail.com>
2021-01-03 18:25:30 -0600

    changed login button text

--- src/js/pages/loginPage.js
+++ src/js/pages/loginPage.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import SignInButton from '../components/SignInButton';
 
-const LoginPage = () => (
-  <div id="app" className="d-flex flex-column justify-content-center align-items-center">
-    <div className="sign-in-icon-wrapper1">
-      <div className="sign-in-icon-wrapper2">
-        <div className="sign-in-icon-wrapper3">
-          <FontAwesomeIcon className="sign-in-icon" icon="feather-alt" />
-        </div>
-      </div>
+const LoginPage = () => {
+  // icon animation shows on render, wait for user to focus on page before rendering
+  const [showIcon, setShowIcon] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener('focus', () => setShowIcon(true));
+    return window.addEventListener('focus', () => setShowIcon(true));
+  }, []);
+
+
+  return (
+    <div id="app" className="d-flex flex-column justify-content-center align-items-center">
+      {showIcon && (
+        <>
+          <div className="sign-in-icon-wrapper1">
+            <div className="sign-in-icon-wrapper2">
+              <div className="sign-in-icon-wrapper3">
+                <FontAwesomeIcon className="sign-in-icon" icon="feather-alt" />
+              </div>
+            </div>
+          </div>
+          <h1 className="sign-in-title">ReacTrello</h1>
+        </>
+      )}
+      <SignInButton text="Enter" />
     </div>
-    <h1 className="sign-in-title">ReacTrello</h1>
-    <SignInButton text="Continue Anonymously" />
-  </div>
-);
+  );
+};
 
 export default LoginPage;
